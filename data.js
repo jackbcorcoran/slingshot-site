1d5b86ce34327e5b75f444382a0b40d08c8d5aaf
slingshot <jack.b.corcoran@gmail.com>
2026-03-24 13:49:41 -0700

    Update data.js

--- data.js
+++ data.js
@@ -4,10 +4,10 @@
 
 var leagueNote = {
 
-  week: "—",       // e.g.  1, 2, 3 ... or "—" for offseason
+  week: "5",       // e.g.  1, 2, 3 ... or "—" for offseason
   year: "2026",    // season year
 
-  text: ""         // paste your note here between the quotes
+  text: "Don't forget to poop your pants"         // paste your note here between the quotes
                    // use \n for a new line, e.g. "Line one.\nLine two."
 
 };
