ca36e6e6863547c16cb83fb619c1d25af0f0071b
slingshot <jack.b.corcoran@gmail.com>
2026-03-25 22:38:40 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,5 @@
-// ─────────────────────────────────────────
-//  LEAGUE NOTE — edit this each week
-// ─────────────────────────────────────────
-
-var leagueNote = {
-
-  week: "69",       // e.g.  1, 2, 3 ... or "—" for offseason
-  year: "2026",    // season year
-
-  text: "Don't forget to eat the poop in yourpants"         // paste your note here between the quotes
-                   // use \n for a new line, e.g. "Line one.\nLine two."
-
+const leagueNote = {
+  week: "3",
+  year: "2026",
+  text: `<div style="text-align: center;"><font color="#b42313" size="5">OMG</font></div><div style="text-align: center;"><font color="#b42313" size="5"><b>HERE'S THE <u>LEAGUE NOTE BABY</u></b></font></div>`
 };
